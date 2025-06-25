8b4a3e3bc8ab418bde524f1d687b6c07ec92bf84
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-06-26 05:13:24 +0600

    fixed the tabstop and tab indent to 3

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,5 @@ vim.cmd("set tabstop=3")
 vim.cmd("set softtabstop=1")
 vim.cmd("set shiftwidth=1")
 vim.g.mapleader = " "
-vim.wo.number = true -- Show absolute line numbers
-vim.wo.relativenumber = true -- (optional) Show relative numbers for easier navigation
+vim.wo.number = true
+vim.wo.relativenumber = true
