2aa43f700fecef6c0aea3b2ad60904d9b32786e8
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-06-25 06:11:39 +0600

    there is lualine updated...

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.opt.termguicolors = true
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
+vim.cmd("set tabstop=3")
 vim.cmd("set softtabstop=1")
 vim.cmd("set shiftwidth=1")
 vim.g.mapleader = " "
