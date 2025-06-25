29bf145dae7ab331eb721e57dde6370624a54c93
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-06-26 05:17:17 +0600

    style lua indent set to 3

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,26 +6,19 @@ return {
 
   null_ls.setup({
    sources = {
-    -- Formatters
     null_ls.builtins.formatting.prettier.with({
      extra_args = { "--tab-width", "3" },
-    }), -- for JS, TS, React, etc
-
-    -- You can add more:
-    null_ls.builtins.formatting.stylua, -- Lua
-    -- null_ls.builtins.formatting.black,   -- Python
-    -- Diagnostics (optional)
-    -- null_ls.builtins.diagnostics.eslint,
+    }),
+    null_ls.builtins.formatting.stylua.with({
+     extra_args = { "--tab-width", "3" },
+    }), -- Lua
    },
 
    on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
-     -- Format keymap
      vim.keymap.set("n", "<leader>f", function()
       vim.lsp.buf.format({ async = false })
      end, { buffer = bufnr, desc = "Format file" })
-
-     -- Auto format on save (optional, enable if you want)
      vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       callback = function()
